a372d3e1f3357c9b7e90422ecca203c654117a2f
Ygmacio Maza <153003448+ygmzsz@users.noreply.github.com>
2025-11-28 10:43:09 -0500

    Created Database Script and Added test Data

---
+++ Scripts/CreateDatabase.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE FDMS;
+GO
+
+USE FDMS;
+GO
+
+CREATE TABLE GForceParameters
+¨(
+	Tail_number VARCHAR(20) NOT NULL,
+	Packet_sequence INT NOT NULL,
+	Aircraft_timestamp DATETIME NOT NULL,
+	Accel_x DECIMAL(10,6) NOT NULL,
+	Accel_y DECIMAL(10,6) NOT NULL,
+	Accel_z DECIMAL(10,6) NOT NULL,
+	Weight DECIMAL(12,4) NOT NULL,
+	Stored_timestamp DATETIME DEFAULT GETDATE(),
+	PRIMARY KEY (Tail_number, Packet_sequence)
+);
+GO
+
+CREATE TABLE AttitudeParameters
+(
+	Tail_number VARCHAR(20) NOT NULL,
+	Packet_sequence INT NOT NULL,
+	Aircraft_timestamp DATETIME NOT NULL,
+	Altitude DECIMAL(12,4) NOT NULL,
+	Pitch DECIMAL(10,6) NOT NULL,
+	Bank DECIMAL(10,6) NOT NULL,
+	Weight DECIMAL(12,4) NOT NULL,
+	Stored_timestamp DATETIME DEFAULT GETDATE(),
+	PRIMARY KEY (Tail_number, Packet_sequence)
+);
+GO
+
+CREATE TABLE ErrorLog
+(
+	Tail_number VARCHAR(20) NOT NULL,
+	Packet_sequence INT NOT NULL,
+	Raw_packet VARCHAR(MAX) NOT NULL,
+	Stored_timestamp DATETIME DEFAULT GETDATE(),
+	PRIMARY KEY (Tail_number, Packet_sequence)
+);
+GO
+
+INSERT INTO GForceParameters (Tail_number, Packet_sequence, Aircraft_timestamp, Accel_x, Accel_y, Accel_z, Weight)
+Values ('C-QWWT', 1, GETDATE(), 0.0, 0.0, 0.0, 15000.0000);
+
+INSERT INTO AttitudeParameters (Tail_number, Packet_sequence, Aircraft_timestamp, Altitude, Pitch, Bank, Weight)
+Values ('C-QWWT', 1, GETDATE(), 35000.0000, 2.5000, 0.0000, 15000.0000);
