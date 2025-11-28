67f14eef08a9ead2f765cb928a2a7833f8d9b170
Ygmacio Maza <153003448+ygmzsz@users.noreply.github.com>
2025-11-28 11:35:09 -0500

    Added the Base Data Service Class

---
+++ Data/DataBaseConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FDMS.GroundTerminal.Data
+{
+    public static class DataBaseConfiguration
+    {
+        public static string GetConnectionString()
+        {
+            var setting = ConfigurationManager.ConnectionStrings["FDMSConnection"];
+
+            if (setting != null)
+            {
+                return setting.ConnectionString;
+            }
+
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = @"localhost\SQLEXPRESS",
+                InitialCatalog = "FDMS",
+                IntegratedSecurity = true,
+                TrustServerCertificate = true
+            };
+            return builder.ConnectionString;
+        }
+    }
+}
---
+++ Services/IDataBaseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FDMS.GroundTerminal.Models;
+
+namespace FDMS.GroundTerminal.Services
+{
+    public interface IDataBaseService
+    {
+        DatabaseStatus TestConnection();
+
+        bool StoreTelemetry(TelemetryRecord record);
+
+        bool StoreInvalidPacket(InvalidPacket record);
+
+        IList<string> GetTailNumbers();
+
+        IList<TelemetryRecord> SearchTelemetry(string tailNumber, DateTime from, DateTime to);
+        IList<InvalidPacket> SearchInvalidPackets(string tailNumber, DateTime from, DateTime to);
+
+        TelemetryRecord GetLatestTelemetry(string tailNumber);
+    }
+}
